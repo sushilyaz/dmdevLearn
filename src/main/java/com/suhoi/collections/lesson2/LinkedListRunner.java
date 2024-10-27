4cf1a07e14e52593526cf1079ca85fee7015f27b
suhoi <suhoio2k00@gmail.com>
2024-10-27 19:27:11 +0300

    collections - LinkedList

---
+++ src/main/java/com/suhoi/collections/lesson2/LinkedListRunner.java
@@ -0,0 +1,11 @@
+package com.suhoi.collections.lesson2;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListRunner {
+    public static void main(String[] args) {
+        List<Integer> linkedList = new LinkedList<Integer>();
+        // если вставка в начало или в середину -
+    }
+}
